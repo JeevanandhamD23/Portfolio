718cb8f38fde1b95b2efef11999a8341a391422f
JeevanandhamD <djeevanandham553@gmail.com>
2024-10-05 19:17:19 +0530

    updated footer and package

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/JeevanandhamD23/Portfolio">
           <div>Designed &amp; Built by Jeevanandham D</div>
 
           {githubInfo.stars && githubInfo.forks && (
